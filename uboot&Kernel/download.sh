2674a2360b0d0b7d39dd62efeb090a9c6a99e2cb
Pierre-jean TEXIER <ouf_du_45@hotmail.com>
2013-11-23 19:59:07 +0100

    Update download.sh

--- uboot&Kernel/download.sh
+++ uboot&Kernel/download.sh
@@ -9,14 +9,10 @@
 
 mkdir a20_kernel_3.4_DRONE/
 cd a20_kernel_3.4_DRONE/
-
-git clone -b sunxi https://github.com/linux-sunxi/u-boot-sunxi.git && cd u-boot-sunxi/
-
-make A20-OLinuXino_MICRO CROSS_COMPILE=/home/$USERNAME/PROJET_Drone/gcc-linaro-arm-linux-gnueabihf-4.7-2013.01-20130125_linux/bin/arm-linux-gnueabihf- -j2
-
+git clone -b sunxi https://github.com/linux-sunxi/u-boot-sunxi.git
+cd u-boot-sunxi/
+make A20-OLinuXino_MICRO_config CROSS_COMPILE=/home/texierp/PROJET_Drone/gcc-linaro-arm-linux-gnueabihf-4.7-2013.01-20130125_linux/bin/arm-linux-gnueabihf- -j2
 cd ..
-
 git clone https://github.com/texierp/linux-sunxi.git && cd linux-sunxi/
-
 make sun7i_defconfig
 make ARCH=arm xconfig
